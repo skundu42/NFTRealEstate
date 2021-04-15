51409ff988d62b893f76dcb0118ad94eefa9a2b1
skundu42 <skundu_be19@thapar.edu>
2021-04-15 14:47:55 +0530

    contract and tests

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,87 @@
-pragma solidity >=0.4.21 <0.6.0;
+//pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.4.21;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./verifier.sol";
+import "./ERC721Mintable.sol";
 
+contract SolnSquareVerifier is ERC721MintableComplete {
 
+    // unique solutions - key = keccak256(abi.encodePacked( arguments for Verifier.verifyTx)
+    mapping (bytes32 => bool) private _solutions;
+    uint private _solutionCnt;
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
-
-
-
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
+    // Verifier
+    Verifier private _contractVerifier;
 
 
+    event SolutionAdded(uint counter);
 
-// TODO Create an event to emit when a solution is added
+    constructor(string memory name_, string memory symbol_, address verifierAddress) public ERC721MintableComplete(name_, symbol_) {
+        _contractVerifier = Verifier(verifierAddress);
+        _solutionCnt = 0;
+    }
 
+    /// @return number of solutions
+    function numSolution() public view returns(uint){
+        return _solutionCnt;
+    }
 
+    // only required to do a test as perthe UDacity requirements
+    /// @dev check if (the hash of) a solution is already known, and if not, add it
+    /// @return true if a new (previously not present) solution is added, false otherwise
+    function addSolution(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    )
+        public
+        onlyOwner
+        whenNotPaused
+        returns(bool)
+    {
+        // compute hash of proof            
+        bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+        
+        return _addSolution(key);                              
+    }
 
-// TODO Create a function to add the solutions to the array and emit the event
+    /// @dev check if (the hash of) a solution is already known, and if not, add it
+    /// @return true if a new (previously not present) solution is added, false otherwise
+    function _addSolution(bytes32 key) private returns(bool){
+        if(_solutions[key]==false){
+            _solutions[key] = true;
+            _solutionCnt++;
+            emit SolutionAdded(_solutionCnt);
+            return true;
+        }else{
+            return false;
+        }                      
+    }
 
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /// @dev mint a new token only if a valid proof, not used previously, is passed (a, b, c, input)
+    function mint(
+        address to,
+        uint256 tokenId,
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    )
+        public
+        whenNotPaused
+        returns(bool)
+    {
+        // compute hash of proof            
+        bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+        // hash of proof cannot be present
+        require(_solutions[key] == false);
+        // is it a valid solution?
+        if(_contractVerifier.verifyTx(a,b,c,input)){
+            _addSolution(key);
+            return mint(to,tokenId);
+        }else{
+            return false;
+        }         
+    }
+}
